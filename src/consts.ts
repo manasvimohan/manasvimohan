d3dcbf53cfa3259a0983a80650ee91289a0b44a8
MiniPC <manasvimsharma@gmail.com>
2025-01-20 18:24:48 +0530

    Good Changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tool Hub';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Manasvi.co.in – Your Ultimate Blog Hub for Tech, Lifestyle, and More';
+export const SITE_DESCRIPTION = `Welcome to Manasvi.co.in – your ultimate destination for expertly curated blogs across a world of topics! From the latest in AI, blockchain, and quantum computing to digital marketing strategies, online income ideas, and side hustle success stories, this platform is designed to inspire and empower. Explore travel adventures, food discoveries, health tips, and lifestyle hacks alongside guides on business growth, skill-building courses, and personal development. Whether you're a tech enthusiast, a budding entrepreneur, or someone seeking inspiration to level up your life, Manasvi.co.in has something for everyone. Dive in and start your journey of discovery, growth, and success today!`;
+export const SITE_DESCRIPTION_FB = `Discover expertly curated blogs on AI, blockchain, quantum computing, digital marketing, online income, travel, food, health, and more at Manasvi.co.in. Your go-to destination for inspiration, learning, and growth!`;
+export const SITE_DESCRIPTION_TW = `Explore AI, blockchain, quantum computing, online income, side hustles, travel, health, food, and more on Manasvi.co.in. Inspiring blogs to inform, entertain, and empower!`
