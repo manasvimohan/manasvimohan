bff9853b381578d4083920baddead403f08f61d6
MiniPC <manasvimsharma@gmail.com>
2025-01-20 14:29:00 +0530

    Interim

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://manasvi.co.in',
+	integrations: [ mdx(), sitemap() ],
+//   markdown: {
+//     remarkPlugins: [ [remarkToc, { heading: 'toc', maxDepth: 3 } ] ],
+//   },
 });
